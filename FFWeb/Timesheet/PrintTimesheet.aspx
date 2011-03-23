f2305d3fd858677d0e02a071dfc107d82102e4a0
mc97 <smile.kaldra@gmail.com>
2011-03-23 15:46:23 -0700

    Print time sheet.
    still need a way go back more than one week

--- FFWeb/Timesheet/PrintTimesheet.aspx
+++ FFWeb/Timesheet/PrintTimesheet.aspx
@@ -1,7 +1,13 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FlyingFishMasterPage.master" AutoEventWireup="true" CodeFile="PrintTimesheet.aspx.cs" Inherits="Timesheet_PrintTimesheet" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FlyingFishMasterPage.master" AutoEventWireup="true"
+    CodeFile="PrintTimesheet.aspx.cs" Inherits="Timesheet_PrintTimesheet" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="Server">
+    
+    <asp:GridView ID="gvPrintTimesheet" runat="server">
+    </asp:GridView>
+    
+    <asp:Button ID="btnGetTimesheet" runat="server" Text="Get Timesheet" 
+        onclick="btnGetTimesheet_Click" />
 </asp:Content>
-
--- FFWeb/Timesheet/PrintTimesheet.aspx.cs
+++ FFWeb/Timesheet/PrintTimesheet.aspx.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FFLib;
 
 public partial class Timesheet_PrintTimesheet : System.Web.UI.Page
 {
+    FlyingFishClassesDataContext ff = new FlyingFishClassesDataContext();
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+
+    protected void btnGetTimesheet_Click(object sender, EventArgs e)
+    {
+        //Shows previous week's timesheet
+        //needs a session object to hold the empId
+        
+
+        System.DateTime now = System.DateTime.Now.Date;
+        System.DateTime pre = now.AddDays(-7);
+        var qry = from o in ff.TimeSheetEntries
+                  where o.empId == 1 && o.tsDate < now && o.tsDate > pre
+                  select new
+                  {
+                      Project = o.projId,
+                      WP = o.wpId,
+                      total = o.sun + o.mon + o.tue + o.wed + o.thu + o.fri + o.sat,
+                      Sun = o.sun,
+                      Mon = o.mon,
+                      Tue = o.tue,
+                      Wed = o.wed,
+                      Thu = o.thu,
+                      Fri = o.fri,
+                      Sat = o.sat,
+                      Note = o.notes
+                  };
+
+        gvPrintTimesheet.DataSource = qry;
+        gvPrintTimesheet.DataBind();
     }
 }
