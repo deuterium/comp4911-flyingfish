cb5808a8b90fa0d50da458ff2c3407b5161f6601
mc97 <smile.kaldra@gmail.com>
2011-03-27 18:29:13 -0700

    User can print the time sheet by entering a query date or the system will show the previous week time sheet when no query is entered

--- FFWeb/Timesheet/PrintTimesheet.aspx
+++ FFWeb/Timesheet/PrintTimesheet.aspx
@@ -5,6 +5,14 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="content" runat="Server">
     
+   
+    Start Date:
+    <asp:TextBox ID="tbStartDate" runat="server"></asp:TextBox>
+    <asp:RangeValidator ID="rvDate" runat="server" 
+        ErrorMessage="Invalid format or Not within the valid time period (1900 ~ 2100)" 
+        Type="Date" ControlToValidate="tbStartDate" ForeColor="Red" 
+        MaximumValue="2100/12/31" MinimumValue="1900/1/1"></asp:RangeValidator>
+    
     <asp:GridView ID="gvPrintTimesheet" runat="server">
     </asp:GridView>
     
--- FFWeb/Timesheet/PrintTimesheet.aspx.cs
+++ FFWeb/Timesheet/PrintTimesheet.aspx.cs
@@ -11,7 +11,9 @@ public partial class Timesheet_PrintTimesheet : System.Web.UI.Page
     FlyingFishClassesDataContext ff = new FlyingFishClassesDataContext();
     protected void Page_Load(object sender, EventArgs e)
     {
+        // shows the prvious week timesheet as default
 
+        showPreviousWeekTimesheet();
     }
 
 
@@ -19,12 +21,68 @@ public partial class Timesheet_PrintTimesheet : System.Web.UI.Page
     {
         //Shows previous week's timesheet
         //needs a session object to hold the empId
-        
+        //
 
+        if (tbStartDate.Text == string.Empty)
+        {
+            showPreviousWeekTimesheet();
+        }
+        else
+        {
+            qryTimesheet();
+        }
+
+
+
+    }
+
+    public void showPreviousWeekTimesheet()
+    {
         System.DateTime now = System.DateTime.Now.Date;
         System.DateTime pre = now.AddDays(-7);
         var qry = from o in ff.TimeSheetEntries
                   where o.empId == 1 && o.tsDate < now && o.tsDate > pre
+                  select new
+                  {
+                      Project = o.projId,
+                      WP = o.wpId,
+                      total = o.sun + o.mon + o.tue + o.wed + o.thu + o.fri + o.sat,
+                      Sun = o.sun,
+                      Mon = o.mon,
+                      Tue = o.tue,
+                      Wed = o.wed,
+                      Thu = o.thu,
+                      Fri = o.fri,
+                      Sat = o.sat,
+                      Note = o.notes
+                  };
+
+        gvPrintTimesheet.DataSource = qry;
+        gvPrintTimesheet.DataBind();
+    }
+
+    public void qryTimesheet()
+    {
+        System.DateTime qryDate = Convert.ToDateTime(tbStartDate.Text);
+        System.DateTime actualQryDate = qryDate;
+        System.DayOfWeek dayOfWeek = qryDate.DayOfWeek;
+
+
+        switch (dayOfWeek.ToString())
+        {
+            case "Monday": actualQryDate = qryDate.AddDays(-1); break;
+            case "Tuesday": actualQryDate = qryDate.AddDays(-2); break;
+            case "Wednesday": actualQryDate = qryDate.AddDays(-3); break;
+            case "Thursday": actualQryDate = qryDate.AddDays(-4); break;
+            case "Friday": actualQryDate = qryDate.AddDays(-5); break;
+            case "Saturday": actualQryDate = qryDate.AddDays(-6); break;
+            default: break;
+        }
+
+       
+        System.DateTime pre = qryDate.AddDays(-7);
+        var qry = from o in ff.TimeSheetEntries
+                  where o.empId == 1 && o.tsDate < qryDate && o.tsDate > pre
                   select new
                   {
                       Project = o.projId,
