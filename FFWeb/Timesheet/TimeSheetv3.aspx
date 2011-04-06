833ad2e951cd4464f9b7c8a03b30382f08848b41
saad <saad.shaharyar@gmail.com>
2011-04-06 13:06:34 -0700

    custom timesheet

---
+++ FFWeb/Timesheet/TimeSheetv3.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TimeSheetv3.aspx.cs" Inherits="Timesheet_TimeSheetv3"   EnableEventValidation="false" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="dvTimeSheet" AutoGenerateColumns="False" runat="server" OnRowCancelingEdit="dvTimeSheet_RowCancelingEdit"
+            OnRowEditing="dvTimeSheet_RowEditing" OnRowCommand="dvTimeSheet_RowCommand" OnRowUpdating="dvTimeSheet_RowUpdating">
+            <Columns>
+                <asp:TemplateField HeaderText="ProjID">
+                    <EditItemTemplate>
+                        <asp:Label ID="lblProj" runat="server" Text='<%# Bind("ProjID") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblProj2" runat="server" Text='<%# Bind("ProjID") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Work Package ID">
+                    <EditItemTemplate>
+                        <asp:Label ID="lblWp" runat="server" Text='<%# Bind("WpID") %>'></asp:Label>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblWp2" runat="server" Text='<%# Bind("WpID") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Mon">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbMon" Width="60px" runat="server" Text='<%# Bind("Mon") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblMon" runat="server" Text='<%# Bind("Mon") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Tue">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbTue" Width="60px" runat="server" Text='<%# Bind("Tue") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblTue" runat="server" Text='<%# Bind("Tue") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Wed">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbWed" Width="60px" runat="server" Text='<%# Bind("Wed") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblWed" runat="server" Text='<%# Bind("Wed") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Thur">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbThur" Width="60px" runat="server" Text='<%# Bind("Thur") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblThur" runat="server" Text='<%# Bind("Thur") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Fri">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbFri" Width="60px" runat="server" Text='<%# Bind("Fri") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblFri" runat="server" Text='<%# Bind("Fri") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Sat">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbSat" Width="60px" runat="server" Text='<%# Bind("Sat") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblSat" runat="server" Text='<%# Bind("Sat") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Sun">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbSun" Width="60px" runat="server" Text='<%# Bind("Sun") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblSun" runat="server" Text='<%# Bind("Sun") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Notes">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbNote" Width="60px" runat="server" Text='<%# Bind("Notes") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="lblNote" runat="server" Text='<%# Bind("Notes") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ButtonType="Button" ShowEditButton="True" UpdateText="Update" >
+                
+                    <ItemStyle HorizontalAlign="Center" Wrap="False" />
+                </asp:CommandField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ FFWeb/Timesheet/TimeSheetv3.aspx.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FFLib;
+using System.Data;
+
+public partial class Timesheet_TimeSheetv3 : System.Web.UI.Page
+{
+
+    FlyingFishClassesDataContext ff = new FlyingFishClassesDataContext();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        populategridview();
+
+    }
+
+
+
+    private void populategridview()
+    {
+        string empIdTemp = "";
+        var qry = (from o in ff.EmployeeMemberships
+                   join emp in ff.aspnet_Users on o.userId equals emp.UserId
+                   where emp.UserName == User.Identity.Name
+                   select o.empId).Single();
+
+        Session["CurEmpId"] = qry.ToString();
+        empIdTemp = qry.ToString();
+        //Label1.Text += "\t" + Convert.ToString(Session["CurEmpId"]);
+
+        System.DateTime currentDate = System.DateTime.Now;
+        System.DateTime actualQryDate = currentDate;
+        System.DayOfWeek dayOfWeek = currentDate.DayOfWeek;
+
+        // trying to get the sunday of the week picked by the user
+        switch (dayOfWeek.ToString())
+        {
+            case "Monday": actualQryDate = currentDate.AddDays(-1); break;
+            case "Tuesday": actualQryDate = currentDate.AddDays(-2); break;
+            case "Wednesday": actualQryDate = currentDate.AddDays(-3); break;
+            case "Thursday": actualQryDate = currentDate.AddDays(-4); break;
+            case "Friday": actualQryDate = currentDate.AddDays(-5); break;
+            case "Saturday": actualQryDate = currentDate.AddDays(-6); break;
+            default: break;
+        }
+        Session["CurrentDate"] = actualQryDate;
+
+        var qry2 = from ts in ff.TimesheetEntries
+                   where (ts.tsDate >= actualQryDate) && (ts.empId == Convert.ToInt32(empIdTemp))
+                   select new
+                   {
+                       ProjID = ts.projId,
+                       WpId = ts.wpId,
+                       tsdate = ts.tsDate,
+                       Sun = ts.sun,
+                       Mon = ts.mon,
+                       Tue = ts.tue,
+                       Wed = ts.wed,
+                       Thu = ts.thu,
+                       Fri = ts.fri,
+                       Sat = ts.sat,
+                       Notes = ts.notes
+
+                   };
+
+
+        if (qry2.Count() == 0)
+        {
+            //gvStatus.DataSource = null;
+            //gvStatus.DataBind();
+            //lblResults.Visible = true;
+            //divReportData.Visible = false;
+            //return;
+        }
+
+        DataTable dt = new DataTable();
+        dt.Columns.Add(new DataColumn("ProjID", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("WpID", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Mon", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Tue", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Wed", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Thur", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Fri", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Sat", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Sun", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Notes", typeof(System.String)));
+        dt.Columns.Add(new DataColumn("Date", typeof(System.String)));
+
+
+        foreach (var row in qry2)
+        {
+            DataRow dr = dt.NewRow();
+            dr["ProjID"] = row.ProjID;
+            dr["WpID"] = row.WpId;
+            dr["Mon"] = row.Mon;
+            dr["Tue"] = row.Tue;
+            dr["Wed"] = row.Wed;
+            dr["Thur"] = row.Thu;
+            dr["Fri"] = row.Thu;
+            dr["Sat"] = row.Sat;
+            dr["Sun"] = row.Sun;
+            dr["Notes"] = row.Notes;
+            dr["Date"] = row.tsdate;
+
+            dt.Rows.Add(dr);
+        }
+
+        double totMon = 0;
+        double totTue = 0;
+        double totWed = 0;
+        double totThu = 0;
+        double totFri = 0;
+        double totSat = 0;
+        double totSun = 0;
+
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][2].ToString();
+            totMon += Convert.ToDouble(dt.Rows[i][2].ToString());
+        }
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][3].ToString();
+            totTue += Convert.ToDouble(dt.Rows[i][3].ToString());
+        }
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][4].ToString();
+            totWed += Convert.ToDouble(dt.Rows[i][4].ToString());
+        }
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][5].ToString();
+            totThu += Convert.ToDouble(dt.Rows[i][5].ToString());
+        }
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][6].ToString();
+            totFri += Convert.ToDouble(dt.Rows[i][6].ToString());
+        }
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][7].ToString();
+            totSat += Convert.ToDouble(dt.Rows[i][7].ToString());
+        }
+
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            var b = dt.Rows[i][8].ToString();
+            totSun += Convert.ToDouble(dt.Rows[i][8].ToString());
+        }
+
+
+
+
+
+        // add the total row
+        DataRow totalRow = dt.NewRow();
+        totalRow["ProjID"] = "Total";
+        totalRow["Mon"] = totMon;
+        totalRow["Tue"] = totThu;
+        totalRow["wed"] = totWed;
+        totalRow["Thur"] = totThu;
+        totalRow["Fri"] = totFri;
+        totalRow["Sat"] = totSat;
+        totalRow["Sun"] = totSun;
+
+        dt.Rows.Add(totalRow);
+
+        dvTimeSheet.DataSource = dt;
+        dvTimeSheet.DataBind();
+    }
+
+    protected void dvTimeSheet_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        // Set the row's index to the event's edit index
+        dvTimeSheet.EditIndex = e.NewEditIndex;
+
+        // Redraw the grid view (you can call a method that queries the db and sets/binds the data source)
+        // This shows the edit templates (e.g. the Textboxes for input)
+        populategridview();
+    }
+
+    // DO NOT MAKE A CUSTOM UPDATED FUNCTION! ONLY AN UPDATING FUNCTION!
+
+    protected void dvTimeSheet_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+
+
+        // Get row being updated
+        GridViewRow row = dvTimeSheet.Rows[e.RowIndex];
+        Object tmp1 = e.NewValues["Mon"];
+        Object tmp2 = e.OldValues["Mon"];
+        string ProjID = e.OldValues["ProjID "].ToString();
+        string WPID = e.OldValues["WpID"].ToString();
+        DateTime Date = Convert.ToDateTime(row.Cells[10].Controls[1].ToString());
+        // Another method to get the other, non-editable values
+        String strEmployee = (((Label)row.Cells[0].Controls[1]).Text);
+
+        // Get new values
+        String strEtc = e.NewValues["Active"].ToString();
+
+        var qryUpdate = (from tse in ff.TimesheetEntries
+                         where (tse.projId == Convert.ToInt32(ProjID)) &&
+                         (tse.wpId.Equals(WPID)) &&
+                         (tse.tsDate == Date) &&
+                         (tse.empId == Convert.ToInt32(Session["CurEmpId"]))
+                         select tse).FirstOrDefault();
+        string NewSat = "";
+        string NewSun = "";
+        string NewMon = "";
+        string NewTue = "";
+        string NewWed = "";
+        string NewThur = "";
+        string NewFri = "";
+
+        //  qryUpdate.sat = newSat;
+
+        /*
+         * 
+         * 
+         * insert new values into db
+         * 
+         * 
+         */
+
+        // Reset the edit index (so no columns are being edited)
+        //   dvTimeSheet.EditIndex = -1;
+
+        // Cancel the edit event to return grid view to normal
+        e.Cancel = true;
+
+        // Redraw the grid view (you can call a method that queries the db and sets/binds the data source)
+        populategridview();
+
+    }
+
+    protected void dvTimeSheet_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        // Reset the edit index (so no columns are being edited)
+        dvTimeSheet.EditIndex = -1;
+
+        // Redraw the grid view (you can call a method that queries the db and sets/binds the data source)
+        populategridview();
+    }
+
+    // Not sure I used this one...
+    protected void dvTimeSheet_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        // Redraw the grid view (you can call a method that queries the db and sets/binds the data source)
+        populategridview();
+    }
+}
